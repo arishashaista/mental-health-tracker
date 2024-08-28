6005ecbf0dc8de221833ab80f6388c54d25f5997
arishashaista <arishashaistaa@gmail.com>
2024-08-28 14:20:07 +0700

    menambahkan URL Deployment PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v_lccqr(mksgq((ihf@9iur$s_nkhud6d^&75fzpfjuvaj)cjx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "arisha-shaista-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
